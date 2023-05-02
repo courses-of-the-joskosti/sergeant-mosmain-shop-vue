544f29610a94ca88279fb6c2b7f80dc41df454c6
Mosmain <mosmain@yandex.ru>
2023-05-02 16:55:18 +0300

    add axios base files

---
+++ src/axios/types.ts
@@ -0,0 +1,40 @@
+export interface RootState {
+  // определите типы состояния здесь
+}
+
+export interface Users {
+  id: number
+  username: string
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+}
+
+export interface Categories {
+  id: number
+  name: string
+  image_url: string
+}
+
+export interface Product {
+  id: number
+  category_id: number
+  name: string
+  description: string
+  brand: string
+  manufacturer: string
+  image_url: string
+  price: number
+}
+
+export interface Carts {
+  id: number
+  user_id: number
+  products: {
+    product_id: number
+    price: number
+    quantity: number
+  }
+  total_price: number
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { store } from './store'
+import { store } from './store/store'
+import vuetify from "./plugins/vuetify";
 
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(vuetify)
 app.mount('#app')
