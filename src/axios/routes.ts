22bff1e449204e9ae0541620ba17b9758c360f9d
Mosmain <mosmain@yandex.ru>
2023-05-08 01:22:20 +0300

    fix routes

--- src/axios/routes.ts
+++ src/axios/routes.ts
@@ -1,3 +1,3 @@
 export const getProducts = () => "/products"
-export const getProductsId = (id: Number) => `/products?id=${id}`
+export const getProductId = (id: Number) => `/products?id=${id}`
 export const getProductsName = (name: String) => `/products?name_like=${name}`
