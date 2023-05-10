c454261009464c2a4ce8f1591ac730fac19fc01e
Mosmain <mosmain@yandex.ru>
2023-05-10 04:27:44 +0300

    navbar with cart badge

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <script lang="ts">
-export default ({
-  data() {}
-});
+import { mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-home',
+          text: 'Home',
+          link: '/'
+        },
+        {
+          icon: 'mdi-dots-grid',
+          text: 'catalog',
+          link: '/catalog'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState(['cart']),
+    cartItemsCount(): number {
+      return this.cart.length
+    }
+  }
+}
 </script>
 
 <template>
-  <div theme="dark">
-    <nav>
-      <router-link to="/">Home</router-link> | 
-      <router-link to="/catalog">Catalog</router-link>
-    </nav>
-    <main>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>Name</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn
+          v-for="(item, i) in items"
+          :key="i"
+          class="mx-2 h-100"
+          rounded="0"
+          size="large"
+          :to="item.link"
+        >
+          <v-icon start :icon="item.icon"></v-icon>
+          {{ item.text }}
+        </v-btn>
+        <v-btn class="mx-2 h-100" rounded="0" size="large" to="/cart">
+          <v-badge v-if="cartItemsCount !== 0" :content="cartItemsCount" color="error" class="mr-3">
+            <v-icon start icon="mdi-cart"></v-icon>
+          </v-badge>
+          <v-icon v-else start icon="mdi-cart"></v-icon>
+          cart
+        </v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
       <router-view />
-    </main>
-  </div>
+    </v-main>
+  </v-app>
 </template>
