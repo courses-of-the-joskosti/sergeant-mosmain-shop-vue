c9993f3dbe4b9647f4d048c61bfc6bd94f4bff7b
Mosmain <mosmain@yandex.ru>
2023-05-10 04:24:26 +0300

    little edit in routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 
-export default createRouter({
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductView',
+    component: () => import('@/views/ProductView.vue'),
+    props: (route: { params: { id: Number; }; }) => ({ id: Number(route.params.id) }),
+  },
+  {
+    path: '/catalog',
+    component: () => import('@/views/CatalogView.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: () => import('@/views/NotFoundView.vue')
+  }
+]
+
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/product/:id',
-      name: 'ProductView',
-      component: () => import('@/views/ProductView.vue'),
-      props: (route) => ({ id: Number(route.params.id) })
-    },
-    {
-      path: '/catalog',
-      component: () => import('@/views/CatalogView.vue')
-    },
-    {
-      path: '/:catchAll(.*)',
-      component: () => import('@/views/NotFoundView.vue')
-    }
-  ]
+  routes
 })
+
+// router.beforeEach((to, from, next) => {
+//   if (to.meta.title) {
+//     document.title = to.meta.title;
+//   } else {
+//     document.title = 'CeramicShop';
+//   }
+//   next();
+// });
+
+export default router;
