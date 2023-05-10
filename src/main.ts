d383ba9024b851169b82d228c434f61711eba3b4
Mosmain <mosmain@yandex.ru>
2023-05-10 04:24:05 +0300

    beautify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { store } from './store/store'
-import vuetify from "./plugins/vuetify";
-
-// import './assets/main.css'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store/store'
+import vuetify from "@/plugins/vuetify";
 
 const app = createApp(App)
 
