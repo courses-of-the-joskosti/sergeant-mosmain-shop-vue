9756bda1da6c576e15ad3228f322aa8cf5486b67
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-12 00:22:48 +0300

    delete from cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,35 +3,104 @@ import { defineComponent } from 'vue'
 import type { Product } from '@/axios/types'
 import { mapActions, useStore } from 'vuex'
 
-
 export default defineComponent({
   name: 'CartView',
+  data() {
+    return {
+      selectedProducts: [] as number[]
+    }
+  },
   computed: {
     cart(): Product[] {
-      // Получение данных из хранилища (cookie) через Vuex
       const store = useStore()
       return store.state.cart
     }
   },
   methods: {
-    ...mapActions(['removeFromCart'])
+    ...mapActions(['removeFromCart']),
+    deleteSelection() {
+      // Удаляем выбранные продукты из хранилища Vuex
+      this.selectedProducts.forEach((productId) => {
+        this.removeFromCart(productId)
+      })
+
+      // Очищаем массив выбранных идентификаторов
+      this.selectedProducts = []
+    },
+    toggleSelection(productId: number) {
+      if (this.selectedProducts.includes(productId)) {
+        // Если товар уже выбран, удаляем его из массива
+        const index = this.selectedProducts.indexOf(productId)
+        this.selectedProducts.splice(index, 1)
+      } else {
+        // Если товар еще не выбран, добавляем его в массив
+        this.selectedProducts.push(productId)
+      }
+      console.log(this.selectedProducts)
+    }
   }
 })
 </script>
 
 <template>
-  <div>
-    <h2>Корзина</h2>
-    <ul>
-      <li v-for="product in cart" :key="product.id">
-        <!-- Отображение информации о товаре -->
-        <span>{{ product.name }}</span> |
-        <span>{{ product.price }}</span> |
-        <span>{{ product.quantity }} шт.</span> |
+  <v-container grid-list-xs>
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <h2>Корзина</h2>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-row>
+      <v-col cols="8" sm="12" lg="8">
+        <v-container fluid>
+          <v-row>
+            <v-card>
+              <v-btn color="red" @click="deleteSelection">Удалить выбранные</v-btn>
+              <v-card
+                v-for="product in cart"
+                :key="product.id"
+                variant="tonal"
+                theme="light"
+                class="d-flex flex-no-wrap justify-space-between pa-2 ma-2"
+              >
+                <v-card-actions>
+                  <v-checkbox-btn
+                    @change="toggleSelection(product.id)"
+                    :value="product.id"
+                  ></v-checkbox-btn>
+                </v-card-actions>
+                <v-avatar class="ma-3" size="150" rounded="0">
+                  <v-img :aspect-ratio="1 / 1" :src="product.image_url"></v-img>
+                </v-avatar>
 
-        <!-- Кнопка для удаления товара -->
-        <button @click="removeFromCart(product.id)">Удалить</button>
-      </li>
-    </ul>
-  </div>
+                <v-card-text class="flex-wrap pa-0">
+                  <p>{{ product.name }}</p>
+
+                  <v-card-subtitle class="pa-0">
+                    Страна: {{ product.manufacturer }}
+                  </v-card-subtitle>
+                </v-card-text>
+
+                <span>{{ product.price }}</span>
+
+                <v-btn
+                  class="ms-2"
+                  variant="outlined"
+                  size="small"
+                  @click="removeFromCart(product.id)"
+                >
+                  Удалить
+                </v-btn>
+              </v-card>
+            </v-card>
+          </v-row>
+        </v-container>
+      </v-col>
+
+      <v-col cols="4" sm="12" lg="4">
+        <v-card>Make an order block</v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
