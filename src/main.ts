e4083c9f8305358fe7231066036c52b0daa98c2b
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-11 22:47:21 +0300

    add base.css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from '@/router'
 import store from '@/store/store'
 import vuetify from "@/plugins/vuetify";
 
+import '@/assets/base.css'
+
 const app = createApp(App)
 
 app.use(router)
