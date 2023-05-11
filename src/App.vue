ea8ee8b56eda960acbd0478fc3418e65a4b0d40e
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-11 22:47:05 +0300

    beautify App.vue

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,20 @@ export default {
           {{ item.text }}
         </v-btn>
         <v-btn class="mx-2 h-100" rounded="0" size="large" to="/cart">
-          <v-badge v-if="cartItemsCount !== 0 && cartItemsCount < 10" :content="cartItemsCount" color="error" class="mr-3">
+          <v-badge
+            v-if="cartItemsCount !== 0 && cartItemsCount < 10"
+            :content="cartItemsCount"
+            color="error"
+            class="mr-3"
+          >
             <v-icon start icon="mdi-cart"></v-icon>
           </v-badge>
-          <v-badge v-else-if="cartItemsCount > 9" :content="cartItemsCount" color="error" class="mr-5">
+          <v-badge
+            v-else-if="cartItemsCount > 9"
+            :content="cartItemsCount"
+            color="error"
+            class="mr-5"
+          >
             <v-icon start icon="mdi-cart"></v-icon>
           </v-badge>
           <v-icon v-else start icon="mdi-cart"></v-icon>
@@ -58,7 +68,13 @@ export default {
     </v-app-bar>
 
     <v-main>
-      <router-view />
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
