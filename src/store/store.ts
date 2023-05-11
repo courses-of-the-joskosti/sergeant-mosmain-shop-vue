831ead6f020caa703830c068a4f7a932641cb6bf
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-11 18:51:09 +0300

    save state in cookies

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import type { Product } from '@/axios/types'
+import Cookies from 'js-cookie'
 
 interface State {
   cart: Product[]
@@ -14,10 +15,20 @@ export default createStore<State>({
       const index = state.cart.findIndex((p) => p.id === product.id)
       if (index !== -1) {
         state.cart[index].quantity += 1
-        state.cart[index].total_price = state.cart[index].quantity * state.cart[index].price
+        state.cart[index].total_price =
+          state.cart[index].quantity * state.cart[index].price
       } else {
         const item = { ...product, quantity: 1, total_price: product.price }
         state.cart.push(item)
+      }
+      // Сохраняем состояние в cookies после каждого изменения
+      Cookies.set('cart', JSON.stringify(state.cart))
+    },
+    setCartFromCookies(state) {
+      // Чтение состояния из cookies при инициализации хранилища
+      const cart = Cookies.get('cart')
+      if (cart) {
+        state.cart = JSON.parse(cart)
       }
     }
   },
@@ -25,5 +36,8 @@ export default createStore<State>({
     addToCart({ commit }, product: Product) {
       commit('addToCart', product)
     }
-  }
+  },
+  // Вызовите мутацию setCartFromCookies после инициализации хранилища
+  // чтобы восстановить состояние из cookies
+  plugins: [(store) => store.commit('setCartFromCookies')]
 })
