c26d1631cfa31000d46704333b6c58d9bd1fd7dd
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-17 08:19:48 +0300

    add new const for categories

--- src/axios/routes.ts
+++ src/axios/routes.ts
@@ -1,3 +1,7 @@
 export const getProducts = () => "/products"
 export const getProductId = (id: Number) => `/products?id=${id}`
+export const getProductCategoryId = (id: Number) => `/products?category_id=${id}`
 export const getProductsName = (name: String) => `/products?name_like=${name}`
+
+export const getCategories = () => `/categories`
+export const getCategoryId = (id: Number) => `/categories?id=${id}`
