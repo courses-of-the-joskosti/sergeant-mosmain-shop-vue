633f6f9d6a269148351f6f4cd0683549715be84a
Moskvichev Sergey <mosmain@yandex.ru>
2023-05-17 08:20:06 +0300

    init api/index.ts

---
+++ src/api/index.ts
@@ -0,0 +1,23 @@
+import axios from '@/axios/axiosInstance'
+import { getProducts, getProductsName, getCategories, getProductCategoryId } from '@/axios/routes'
+import type { Product, Categories } from '@/axios/types'
+
+export async function fetchProducts(): Promise<Product[]> {
+  const response = await axios.get(getProducts())
+  return response.data
+}
+
+export async function fetchProductsByName(searchQuery: string): Promise<Product[]> {
+  const response = await axios.get(getProductsName(searchQuery), { params: { search: searchQuery } })
+  return response.data
+}
+
+export async function fetchCategories(): Promise<Categories[]> {
+  const response = await axios.get(getCategories())
+  return response.data
+}
+
+export async function fetchProductsByCategoryId(categoryId: number): Promise<Product[]> {
+  const response = await axios.get(getProductCategoryId(categoryId))
+  return response.data
+}
